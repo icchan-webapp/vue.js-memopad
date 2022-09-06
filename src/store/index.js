1a5479fcb98451a5d5c8025d9f5444e774009e20
icchan-webapp <issei.0157@gmail.com>
2022-09-06 17:50:09 +0900

    feat: HomeView.vueにメモ一覧を追加

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,25 @@
 import { createStore } from "vuex";
 
-export default createStore ({
+export default createStore({
   state: {
     count: 0,
-    memos: []
+    memos: [],
   },
   getter: {
+    getCount: (state) => {
+      return state.memos.length;
+    },
+    getAll: (state) => {
+      return state.memos;
+    },
   },
   mutations: {
     // メモを保存
-    save (state, newMemo) {
-      newMemo.id = ++state.count
-      state.memos.unshift(newMemo)
-    }
+    save(state, newMemo) {
+      newMemo.id = ++state.count;
+      state.memos.unshift(newMemo);
+    },
   },
-  actions: {
-  },
-  modules: {
-  },
-})
+  actions: {},
+  modules: {},
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <ul v-if="hasMemos">
+      <li v-for="memo in memos" :key="memo.id">{{ memo.title }}</li>
+    </ul>
+    <p v-else>メモはありません。</p>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "HomeView",
+  computed: {
+    hasMemos() {
+      return this.$store.getters.getCount;
+    },
+    memos() {
+      return this.$store.getters.getAll;
+    },
+  },
+};
+</script>
