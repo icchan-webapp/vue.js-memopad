557580d45a8ecd0f05a1e37c1c3dad843a8eb128
icchan-webapp <issei.0157@gmail.com>
2022-09-06 19:27:56 +0900

    fix: HomeView.vueの文法ミスの修正

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <ul v-if="hasMemos">
       <li v-for="memo in memos" :key="memo.id">
-        <router-link :to="{ name: 'edit', params: { id: memo.id } }">
+        <router-link :to="{ name: 'edit', params: { id: memo.id }}">
           {{ memo.title }}
         </router-link>
       </li>
